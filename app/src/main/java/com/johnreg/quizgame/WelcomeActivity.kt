d5f1a8b26abb3ab83d960ab6a0ed54d0c2454506
John Reg <johnreginaldjao@gmail.com>
2024-03-07 05:12:20 +0800

    Splash Screen Part 2

--- app/src/main/java/com/johnreg/quizgame/WelcomeActivity.kt
+++ app/src/main/java/com/johnreg/quizgame/WelcomeActivity.kt
@@ -1,20 +1,35 @@
 package com.johnreg.quizgame
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.johnreg.quizgame.databinding.ActivityWelcomeBinding
 
 class WelcomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWelcomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_welcome)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Animation
+        val alphaAnimation = AnimationUtils.loadAnimation(applicationContext, R.anim.splash_anim)
+        binding.tvSplash.startAnimation(alphaAnimation)
+
+        // After applying this animation, the WelcomeActivity should be automatically closed
+        // And the MainActivity should be opened, we do this by creating a Handler
+        val handler = Handler(Looper.getMainLooper())
+        // This line of code will hold the process to be 5000 milliseconds, 5 seconds
+        // And then execute the codes in the run function
+        handler.postDelayed({
+            val intent = Intent(this@WelcomeActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 5000)
     }
 }
