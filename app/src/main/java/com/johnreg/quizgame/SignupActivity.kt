34eaf2c7fe829715d1dff4aaa6da118a234272ad
John Reg <johnreginaldjao@gmail.com>
2024-03-08 04:04:13 +0800

    Sign Up Page - Part 2

--- app/src/main/java/com/johnreg/quizgame/SignupActivity.kt
+++ app/src/main/java/com/johnreg/quizgame/SignupActivity.kt
@@ -1,20 +1,18 @@
 package com.johnreg.quizgame
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.johnreg.quizgame.databinding.ActivitySignupBinding
 
 class SignupActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignupBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_signup)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSignup.setOnClickListener {  }
     }
 }
