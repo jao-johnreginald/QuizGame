5fbffcb7aa6cc5664b9e9336d7ae35753be201ba
John Reg <johnreginaldjao@gmail.com>
2024-03-08 03:08:29 +0800

    Sign Up Page - Part 1

--- app/src/main/java/com/johnreg/quizgame/LoginActivity.kt
+++ app/src/main/java/com/johnreg/quizgame/LoginActivity.kt
@@ -1,20 +1,28 @@
 package com.johnreg.quizgame
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.johnreg.quizgame.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSignIn.setOnClickListener {  }
+        binding.btnGoogleSignIn.setOnClickListener {  }
+        binding.tvSignUp.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
         }
+        binding.tvForgotPassword.setOnClickListener {  }
     }
 }
