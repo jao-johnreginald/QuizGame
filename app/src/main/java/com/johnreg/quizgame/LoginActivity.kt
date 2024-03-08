c0ded53b38c4cdf8f66323087c9bf0a2854db79e
John Reg <johnreginaldjao@gmail.com>
2024-03-08 14:23:39 +0800

    Sign In With Firebase - Part 1

--- app/src/main/java/com/johnreg/quizgame/LoginActivity.kt
+++ app/src/main/java/com/johnreg/quizgame/LoginActivity.kt
@@ -2,27 +2,47 @@ package com.johnreg.quizgame
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 import com.johnreg.quizgame.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
 
+    private val auth = FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnSignIn.setOnClickListener {  }
+        binding.btnSignIn.setOnClickListener {
+            val userEmail = binding.etLoginEmail.text.toString()
+            val userPassword = binding.etLoginPassword.text.toString()
+            signInUser(userEmail, userPassword)
+        }
         binding.btnGoogleSignIn.setOnClickListener {  }
         binding.tvSignUp.setOnClickListener {
             val intent = Intent(this, SignupActivity::class.java)
             startActivity(intent)
         }
         binding.tvForgotPassword.setOnClickListener {  }
+    }
+
+    private fun signInUser(userEmail: String, userPassword: String) {
+        // Compare the previously created account with the account currently entered
+        auth.signInWithEmailAndPassword(userEmail, userPassword).addOnCompleteListener { task ->
+            // If the transaction is completed, define the transactions to be performed
+            if (task.isSuccessful) {
+                Snackbar.make(binding.root, "Welcome to Quiz Game", Snackbar.LENGTH_LONG).show()
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                Snackbar.make(binding.root, "${task.exception?.localizedMessage}", Snackbar.LENGTH_LONG).show()
+            }
+        }
     }
 }
