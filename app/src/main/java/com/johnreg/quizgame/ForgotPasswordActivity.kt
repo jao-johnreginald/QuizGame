2e99dfeeea6b08667b3e9415d18371f425739499
John Reg <johnreginaldjao@gmail.com>
2024-03-10 19:27:43 +0800

    Adding the Forgot Password Option - Part 2

--- app/src/main/java/com/johnreg/quizgame/ForgotPasswordActivity.kt
+++ app/src/main/java/com/johnreg/quizgame/ForgotPasswordActivity.kt
@@ -1,18 +1,39 @@
 package com.johnreg.quizgame
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import com.johnreg.quizgame.databinding.ActivityForgotPasswordBinding
 
 class ForgotPasswordActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityForgotPasswordBinding
 
+    private val auth = FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityForgotPasswordBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setButtonListener()
+    }
 
+    private fun setButtonListener() {
+        binding.btnReset.setOnClickListener {
+            val userEmail = binding.etForgotEmail.text.toString()
+            // At this stage the password reset link is sent to the users email address
+            auth.sendPasswordResetEmail(userEmail).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(
+                        applicationContext, "We sent a password reset mail to your email address", Toast.LENGTH_LONG
+                    ).show()
+                    finish()
+                } else {
+                    Toast.makeText(applicationContext, task.exception?.localizedMessage, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 }
