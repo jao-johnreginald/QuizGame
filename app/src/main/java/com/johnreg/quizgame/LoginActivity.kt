130f634ceb4e64a1ca6834fcdc77fcedfec44323
John Reg <johnreginaldjao@gmail.com>
2024-03-10 21:43:24 +0800

    Sign In with Google Account - Part 1

--- app/src/main/java/com/johnreg/quizgame/LoginActivity.kt
+++ app/src/main/java/com/johnreg/quizgame/LoginActivity.kt
@@ -1,15 +1,25 @@
 package com.johnreg.quizgame
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.johnreg.quizgame.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+
+    private lateinit var googleSignInClient: GoogleSignInClient
+
+    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
 
     private val auth = FirebaseAuth.getInstance()
 
@@ -18,16 +28,30 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Access the TextView inside this button
+        val textOfGoogleButton = binding.btnGoogleSignIn.getChildAt(0) as TextView
+        textOfGoogleButton.apply {
+            setText(R.string.continue_with_google)
+            setTextColor(Color.BLACK)
+            textSize = 18F
+        }
+
+        // Register
+        registerActivityForGoogleSignIn()
+
         binding.btnSignIn.setOnClickListener {
             val userEmail = binding.etLoginEmail.text.toString()
             val userPassword = binding.etLoginPassword.text.toString()
             signInUser(userEmail, userPassword)
         }
-        binding.btnGoogleSignIn.setOnClickListener {  }
+
+        binding.btnGoogleSignIn.setOnClickListener { signInGoogle() }
+
         binding.tvSignUp.setOnClickListener {
             val intent = Intent(this, SignupActivity::class.java)
             startActivity(intent)
         }
+
         binding.tvForgotPassword.setOnClickListener {
             val intent = Intent(this, ForgotPasswordActivity::class.java)
             startActivity(intent)
@@ -61,4 +85,23 @@ class LoginActivity : AppCompatActivity() {
         startActivity(intent)
         finish()
     }
+
+    private fun signInGoogle() {
+        // Choose the standard google sign in method with the Builder method
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            // This method specifies that an id token for authenticating users is requested
+            .requestIdToken(getString(R.string.request_id_token))
+            // Ask the user for their email
+            .requestEmail().build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        signIn()
+    }
+
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        // Can't run this intent with startActivity(), use activityResultLauncher
+        activityResultLauncher.launch(signInIntent)
+    }
+
+    private fun registerActivityForGoogleSignIn() {}
 }
