6b81c35a4a30b08dae14a845d7ef69c8e57ec498
John Reg <johnreginaldjao@gmail.com>
2024-03-11 20:02:55 +0800

    Homepage Design

--- app/src/main/java/com/johnreg/quizgame/MainActivity.kt
+++ app/src/main/java/com/johnreg/quizgame/MainActivity.kt
@@ -2,7 +2,10 @@ package com.johnreg.quizgame
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.johnreg.quizgame.databinding.ActivityMainBinding
 
@@ -15,9 +18,25 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.btnStartQuiz.setOnClickListener {
+            val intent = Intent(this, QuizActivity::class.java)
+            startActivity(intent)
+        }
+
         binding.btnSignOut.setOnClickListener {
-            // sign out
+            // Sign out email and password
             FirebaseAuth.getInstance().signOut()
+
+            // Sign out google account
+            val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail().build()
+            val googleSignInClient = GoogleSignIn.getClient(this, gso)
+            googleSignInClient.signOut().addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(applicationContext, "Sign out is successful", Toast.LENGTH_LONG).show()
+                }
+            }
+
             // Close this page and then open the login page when signing out
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
