8e419d0aabef6405edb116be2c293b5813b3121f
John Reg <johnreginaldjao@gmail.com>
2024-04-26 19:27:15 +0800

    Showing Questions in App - Part 1

--- app/src/main/java/com/johnreg/quizgame/QuizActivity.kt
+++ app/src/main/java/com/johnreg/quizgame/QuizActivity.kt
@@ -2,11 +2,25 @@ package com.johnreg.quizgame
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.FirebaseDatabase
 import com.johnreg.quizgame.databinding.ActivityQuizBinding
 
 class QuizActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityQuizBinding
+
+    // Reach the data under the 'questions' child
+    private val database = FirebaseDatabase.getInstance()
+    private val databaseReference = database.reference.child("questions")
+
+    // These will be assigned after the data gets retrieved
+    private var question = ""
+    private var answerA = ""
+    private var answerB = ""
+    private var answerC = ""
+    private var answerD = ""
+    private var correctAnswer = ""
+    private var questionCount = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
