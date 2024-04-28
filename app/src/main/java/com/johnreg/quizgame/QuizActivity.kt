0d2133e769011a3cf477ec2c6c17a74962ca79fb
John Reg <johnreginaldjao@gmail.com>
2024-04-29 00:17:02 +0800

    Getting User's Answers - Part 1

--- app/src/main/java/com/johnreg/quizgame/QuizActivity.kt
+++ app/src/main/java/com/johnreg/quizgame/QuizActivity.kt
@@ -1,5 +1,6 @@
 package com.johnreg.quizgame
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
@@ -28,6 +29,11 @@ class QuizActivity : AppCompatActivity() {
     private var questionCount = 0
     private var questionNumber = 1
 
+    // These will be assigned after an answer is chosen by the user
+    private var userAnswer = ""
+    private var userCorrect = 0
+    private var userWrong = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityQuizBinding.inflate(layoutInflater)
@@ -38,10 +44,62 @@ class QuizActivity : AppCompatActivity() {
         binding.apply {
             btnNext.setOnClickListener { gameLogic() }
             btnFinish.setOnClickListener {  }
-            tvA.setOnClickListener {  }
-            tvB.setOnClickListener {  }
-            tvC.setOnClickListener {  }
-            tvD.setOnClickListener {  }
+            tvA.setOnClickListener {
+                userAnswer = "a"
+                if (correctAnswer == userAnswer) {
+                    tvA.setBackgroundColor(Color.GREEN)
+                    userCorrect++
+                    tvCorrect.text = userCorrect.toString()
+                } else {
+                    tvA.setBackgroundColor(Color.RED)
+                    userWrong++
+                    tvWrong.text = userWrong.toString()
+                    findAnswer()
+                }
+                disableClickableOfOptions()
+            }
+            tvB.setOnClickListener {
+                userAnswer = "b"
+                if (correctAnswer == userAnswer) {
+                    tvB.setBackgroundColor(Color.GREEN)
+                    userCorrect++
+                    tvCorrect.text = userCorrect.toString()
+                } else {
+                    tvB.setBackgroundColor(Color.RED)
+                    userWrong++
+                    tvWrong.text = userWrong.toString()
+                    findAnswer()
+                }
+                disableClickableOfOptions()
+            }
+            tvC.setOnClickListener {
+                userAnswer = "c"
+                if (correctAnswer == userAnswer) {
+                    tvC.setBackgroundColor(Color.GREEN)
+                    userCorrect++
+                    tvCorrect.text = userCorrect.toString()
+                } else {
+                    tvC.setBackgroundColor(Color.RED)
+                    userWrong++
+                    tvWrong.text = userWrong.toString()
+                    findAnswer()
+                }
+                disableClickableOfOptions()
+            }
+            tvD.setOnClickListener {
+                userAnswer = "d"
+                if (correctAnswer == userAnswer) {
+                    tvD.setBackgroundColor(Color.GREEN)
+                    userCorrect++
+                    tvCorrect.text = userCorrect.toString()
+                } else {
+                    tvD.setBackgroundColor(Color.RED)
+                    userWrong++
+                    tvWrong.text = userWrong.toString()
+                    findAnswer()
+                }
+                disableClickableOfOptions()
+            }
         }
     }
 
@@ -82,6 +140,22 @@ class QuizActivity : AppCompatActivity() {
                 Toast.makeText(applicationContext, error.message, Toast.LENGTH_LONG).show()
             }
         })
+    }
+
+    private fun ActivityQuizBinding.findAnswer() {
+        when (correctAnswer) {
+            "a" -> tvA.setBackgroundColor(Color.GREEN)
+            "b" -> tvB.setBackgroundColor(Color.GREEN)
+            "c" -> tvC.setBackgroundColor(Color.GREEN)
+            "d" -> tvD.setBackgroundColor(Color.GREEN)
+        }
+    }
+
+    private fun ActivityQuizBinding.disableClickableOfOptions() {
+        tvA.isClickable = false
+        tvB.isClickable = false
+        tvC.isClickable = false
+        tvD.isClickable = false
     }
 
 }
