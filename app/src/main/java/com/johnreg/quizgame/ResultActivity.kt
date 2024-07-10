cb86aa1c2ab257a3a0d660ed32506f77e38154ec
John Reg <johnreginaldjao@gmail.com>
2024-07-11 07:59:38 +0800

    Preparing the Results - Page Part 1

--- app/src/main/java/com/johnreg/quizgame/ResultActivity.kt
+++ app/src/main/java/com/johnreg/quizgame/ResultActivity.kt
@@ -1,20 +1,31 @@
 package com.johnreg.quizgame
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.johnreg.quizgame.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityResultBinding
+
+    private val database = FirebaseDatabase.getInstance()
+    private val databaseReference = database.reference.child("scores")
+
+    private val auth = FirebaseAuth.getInstance()
+    private val user = auth.currentUser
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_result)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnPlayAgain.setOnClickListener {
+
+        }
+        binding.btnExit.setOnClickListener {
+
         }
     }
 }
